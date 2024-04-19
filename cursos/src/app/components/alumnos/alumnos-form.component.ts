c45367e4e43afb67473a991a471f7a77b7428602
carlo delgado <cdmarallano23@gmail.com>
2024-04-19 01:20:27 -0500

    chore[6]:agregando boton crear tercera parte problemas con los cord

--- cursos/src/app/components/alumnos/alumnos-form.component.ts
+++ cursos/src/app/components/alumnos/alumnos-form.component.ts
@@ -23,7 +23,7 @@ public crear(): void {
   this.service.crear(this.alumno).subscribe(alumno =>{
     console.log(alumno);
     alert(`Alumno ${alumno.nombre} creado con éxitos`);
-    this.router.navigate(['/create-student']);
+    this.router.navigate(['/alumno']);
 
   })
 }
